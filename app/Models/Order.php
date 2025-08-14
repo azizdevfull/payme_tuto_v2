d002e50b8e68a95250562d5b1ff9895a5a0e9cda
Azizbek Isroilov <aziz16110904@gmail.com>
2025-08-14 12:51:19 +0500

    completed base payment template

---
+++ app/Models/Order.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['price', 'product_id', 'status'];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
